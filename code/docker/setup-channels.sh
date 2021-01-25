f96b44babfd37801bfa3b482141b2aa8e76adb22
Andrej Mitrovic <andrej.mitrovich@gmail.com>
2021-01-25 17:40:51 +0900

    wip

--- code/docker/setup-channels.sh
+++ code/docker/setup-channels.sh
@@ -3,7 +3,7 @@
 echo Getting node IDs
 alice_address=$(docker-compose exec -T Alice bash -c "lncli -n regtest getinfo | jq .identity_pubkey")
 bob_address=$(docker-compose exec -T Bob bash -c "lightning-cli getinfo | jq .id")
-wei_address=$(docker-compose exec -T Wei bash -c "eclair-cli -s -j -p eclair getinfo| jq .nodeId")
+wei_address=$(docker-compose exec -T Charlie bash -c "eclair-cli -s -j -p eclair getinfo| jq .nodeId")
 gloria_address=$(docker-compose exec -T Gloria bash -c "lncli -n regtest getinfo | jq .identity_pubkey")
 
 # The jq command returns JSON strings enclosed in double-quote characters
@@ -23,7 +23,7 @@ gloria_address=${gloria_address//\"}
 # Let's tell everyone what we found!
 echo Alice: ${alice_address}
 echo Bob: ${bob_address}
-echo Wei: ${wei_address}
+echo Charlie: ${wei_address}
 echo Gloria: ${gloria_address}
 
 echo Setting up channels...
@@ -31,13 +31,13 @@ echo Alice to Bob
 docker-compose exec -T Alice lncli -n regtest connect ${bob_address}@Bob
 docker-compose exec -T Alice lncli -n regtest openchannel ${bob_address} 1000000
 
-echo Bob to Wei
-docker-compose exec -T Bob lightning-cli connect ${wei_address}@Wei
+echo Bob to Charlie
+docker-compose exec -T Bob lightning-cli connect ${wei_address}@Charlie
 docker-compose exec -T Bob lightning-cli fundchannel ${wei_address} 1000000
 
-echo Wei to Gloria
-docker-compose exec -T Wei eclair-cli -p eclair connect --uri=${gloria_address}@Gloria
-docker-compose exec -T Wei eclair-cli -p eclair open --nodeId=${gloria_address} --fundingSatoshis=1000000
+echo Charlie to Gloria
+docker-compose exec -T Charlie eclair-cli -p eclair connect --uri=${gloria_address}@Gloria
+docker-compose exec -T Charlie eclair-cli -p eclair open --nodeId=${gloria_address} --fundingSatoshis=1000000
 
 echo Get 10k sats invoice from Gloria
 gloria_invoice=$(docker-compose exec -T Gloria bash -c "lncli -n regtest addinvoice 10000 | jq .payment_request")
